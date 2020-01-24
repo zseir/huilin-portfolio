fc20241e7bc2a1771f0c4c7ea74993f61b37da2d
Hui Lin Tay <s10178003@connect.np.edu.sg>
2020-01-24 17:55:16 +0800

    Updates

---
+++ precache-manifest.a41927a83e208485680970e08ba6a9b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "493f8d1d607591eedb20873564eb6f94",
+    "url": "/huilin-portfolio/index.html"
+  },
+  {
+    "revision": "0dccfa30a590b78bb040",
+    "url": "/huilin-portfolio/static/css/2.90172c5a.chunk.css"
+  },
+  {
+    "revision": "bd14eaff80c05fce0a66",
+    "url": "/huilin-portfolio/static/css/main.388d626b.chunk.css"
+  },
+  {
+    "revision": "0dccfa30a590b78bb040",
+    "url": "/huilin-portfolio/static/js/2.d6d14355.chunk.js"
+  },
+  {
+    "revision": "bd14eaff80c05fce0a66",
+    "url": "/huilin-portfolio/static/js/main.998dfc75.chunk.js"
+  },
+  {
+    "revision": "3949e8f3a5d9bc37b7e3",
+    "url": "/huilin-portfolio/static/js/runtime-main.d239e6e2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/huilin-portfolio/precache-manifest.0a8ad007c96a5ef4cbf1c3db260d5c0a.js"
+  "/huilin-portfolio/precache-manifest.a41927a83e208485680970e08ba6a9b5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/huilin-portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
